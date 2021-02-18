0bb46caea6b77ae2ecf850e12a390cb16aa5b309
Tomas Tokar <tomastokar@Tomass-MacBook-Pro.local>
2021-02-17 19:56:57 -0700

    prep data

--- prep_data.py
+++ prep_data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import sqlite3
 #import sqlalchemy 
@@ -15,7 +16,40 @@ for table in tables:
     data[table] = pd.read_sql_query('SELECT * FROM {}'.format(table), conn)
 conn.close()
 
-with open('./data/compas/compas.pkl', 'wb') as f:
-    pkl.dump(data, f)
+cols = ['age', 'race', 'sex', 'priors_count', 'c_charge_degree', 'c_jail_in', 'c_jail_out', 'is_violent_recid']
+d = data['people'][cols].copy()
+d.loc[:,'c_charge_degree'] = d['c_charge_degree'].map(
+    {
+        '(F1)' : 'F', 
+        '(F2)' : 'F', 
+        '(F3)' : 'F', 
+        '(F5)' : 'F',
+        '(F6)' : 'F', 
+        '(F7)' : 'F',
+        '(M1)' : 'M',
+        '(M2)' : 'M',
+        '(M03)' : 'M',
+        '(X)' : 'X', 
+        '(CT)' : 'X', 
+        '(NI0)' : 'X', 
+        '(TCX)' : 'X', 
+        '(F6)' : 'X', 
+        '(CO3)' : 'X'
+    }
+)
+
+d = d[d['c_charge_degree'] != 'X']
+print(d['c_charge_degree'].value_counts())
+#print(d.head())
+
+dt = pd.to_datetime(d['c_jail_out']) - pd.to_datetime(d['c_jail_in'])
+dt = dt / np.timedelta64(1, 'D')
+dt = np.ceil(dt)
+d['length_of_stay'] = dt
+print(d.head())
+
+
+#with open('./data/compas/compas.pkl', 'wb') as f:
+#    pkl.dump(data, f)
 
 
